f871440bd13bcfdb3c00f2d4a7e13948b88d55ea
sujan <sujan@zyoba.com>
2016-08-18 01:21:49 +0545

    initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+	logLevels: {
+        levels: {
+            error: 0,
+            warn: 0,
+            info: 0,
+            debug: 0,
+            cnsl: 0
+        },
+        colors: {
+            error: 'red',
+            warn: 'yellow',
+            info: 'blue',
+            debug: 'white',
+            cnsl: 'green'
+        }
+    },
+    logFolder: '/log/'
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let winston = require('winston');
+let path = require('path');
+let rootPath = path.resolve('.');
+const config = require(rootPath + '/config') || require('./config');
+const fs = require('fs');
+
+(() => {
+    let logDir = config.logFolder;
+    if(!fs.existsSync(logDir)){
+        fs.mkdirSync(logDir);
+    }
+
+    winston.addColors(config.logLevels.colors);
+    winston.cnsl = {};
+
+    let cnsl = new (winston.Logger)({
+        levels: config.logLevels.levels,
+        transports: [
+            new (winston.transports.Console)({colorize: true, prettyPrint: true, depth: 'unlimited'})
+        ]
+    });
+    winston.cnsl.cnslLog = cnsl;
+    winston.cnsl.cnsl = cnsl.cnsl;
+
+    let info = new (winston.Logger)({
+        levels: config.logLevels.levels,
+        transports: [
+            new (winston.transports.Console)({colorize: true, prettyPrint: true, depth: 'unlimited'}),
+            new (winston.transports.File)({name: 'info-file', filename: logDir + 'info.log', level: 'info', logstash: true})
+        ]
+    });
+    winston.cnsl.infoLog = info;
+    winston.cnsl.info = info.info;
+
+    let error = new (winston.Logger)({
+        levels: config.logLevels.levels,
+        transports: [
+            new (winston.transports.Console)({colorize: true, prettyPrint: true, depth: 'unlimited'}),
+            new (winston.transports.File)({name: 'error-file', filename: logDir + 'error.log', level: 'error', logstash: true})
+        ]
+    });
+    winston.cnsl.errorLog = error;
+    winston.cnsl.error = error.error;
+
+    let warn = new (winston.Logger)({
+        levels: config.logLevels.levels,
+        transports: [
+            new (winston.transports.Console)({colorize: true, prettyPrint: true, depth: 'unlimited'}),
+            new (winston.transports.File)({name: 'warn-file', filename: logDir + 'warn.log', level: 'warn', logstash: true})
+        ]
+    });
+    winston.cnsl.warnLog = warn;
+    winston.cnsl.warn = warn.warn;
+
+    let debug = new (winston.Logger)({
+        levels: config.logLevels.levels,
+        transports: [
+            new (winston.transports.Console)({colorize: true, prettyPrint: true, depth: 'unlimited'}),
+            new (winston.transports.File)({name: 'debug-file', filename: logDir + 'debug.log', level: 'debug', logstash: true})
+        ]
+    });
+    winston.cnsl.debugLog = debug;
+    winston.cnsl.debug = debug.debug;
+
+    if(config.cloud){
+		require('winston-loggly');
+	    let cloud = new (winston.Logger)({
+	        levels: config.logLevels.levels,
+	        transports: [
+	            new (winston.transports.Loggly)({inputToken: config.cloud.inputToken, subdomain: config.cloud.subdomain, json: true})
+	        ]
+	    });
+	    winston.cnsl.cloudLog = cloud;
+	    winston.cnsl.cloud = cloud.info;
+    }
+})();
+
+module.exports = winston;
